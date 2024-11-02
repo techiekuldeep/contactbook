5cb902c2cf7eaf8cc2bd7f0eadc589677e8cddd8
Kuldeep Singh <36822070+techiekuldeep@users.noreply.github.com>
2024-11-02 23:19:41 +1300

    Added components skeleton.

---
+++ src/Components/ContactPages/RemoveAllContact.jsx
@@ -0,0 +1,15 @@
+const RemoveAllContact = (props) => {
+    return (
+      <div>
+        <button
+          className="btn btn-danger form-control"
+        //   onClick={() => props.handleRemoveAllContact()}
+        >
+          Remove All
+        </button>
+      </div>
+    );
+  };
+  
+  export default RemoveAllContact;
+
---
+++ src/Components/Layout/Footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+    return (
+      <div
+        style={{
+          color: "gray",
+          marginTop: "10px",
+          borderTop: "1px solid #555",
+          textAlign: "center",
+        }}
+      >
+        Coding is Rhyme! @ContactBook
+      </div>
+    );
+  };
+  
+  export default Footer;
